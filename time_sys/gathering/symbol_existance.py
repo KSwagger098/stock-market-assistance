fe283836f57c747963e06dc5e58ab401005a1b65
Kenneth Albarillo <kenneth.albarillo1973@gmail.com>
2024-09-25 14:00:30 -0700

    Deleted API Keys | NEED TO UPDATE README.md WITH API INSTRUCTIONS

--- time_sys/gathering/symbol_existance.py
+++ time_sys/gathering/symbol_existance.py
@@ -4,8 +4,7 @@ This part of the program checks if a certain stock exists.
 
 import finnhub
 import finnhub.client
-
-API_KEY = 'crmk5p9r01qhaloduv80crmk5p9r01qhaloduv8g'
+from pathlib import Path
 
 
 def stock_exists(stock: str) -> bool:
@@ -13,6 +12,11 @@ def stock_exists(stock: str) -> bool:
     This function gets the current prices of a given list of stocks.
     Returns a dictionary of stocks with it's current price.
     """
+    api_key_path : str = Path.cwd() / 'time_sys' / 'api-codes' / 'finnhub-api.txt'
+    with open(api_key_path) as key_file:
+        API_KEY : str = key_file.read()
+    print(f'stock_exists | Using {API_KEY}')
     finnhub_client = finnhub.Client(api_key=API_KEY)
     outcome = finnhub_client.symbol_lookup(stock)
     return bool(outcome["count"])
+
--- time_sys/my_sending/send_msg.py
+++ time_sys/my_sending/send_msg.py
@@ -3,13 +3,17 @@ This is where the sending functionality is programmed.
 """
 
 import requests
+from pathlib import Path
 
 
 def send(msg : str, chat_id : str) -> None:
     """
     Sends any string message to the user.
     """
-    TOKEN = '7273426065:AAFXQWFSzHhuUNyAEv9DKSuCPdiBJlF85qU'
+    token_path = Path.cwd() / 'time_sys' / 'api-codes' / 'telergam-api.txt'
+    with open(token_path) as token_file:
+        TOKEN : str = token_file.read()
+    print(f'send | Using {TOKEN}')
     url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg}'
     info = requests.get(url).json()
     print(f"SEND FUNCTION | info : {info['result']['text']}\n")
