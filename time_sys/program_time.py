0c81517f6cf17227bdb18ab19f9633384b1decd9
Kenneth Albarillo <kenneth.albarillo1973@gmail.com>
2024-09-25 00:23:39 -0700

    Updated program_time with set paths

--- time_sys/program_time.py
+++ time_sys/program_time.py
@@ -9,6 +9,11 @@ from my_sending import send_msg, format_msg, recieve_msg
 from gathering import stock_price
 from storing import store_data, sort_users
 
+
+STOCK_PATH = Path.cwd()/'time_sys'/'storing'/'stocks.json'
+USER_PATH = Path.cwd()/'time_sys'/'storing'/'users.json'
+
+
 def start() -> None:
     """
     This function begins the program and runs the program until 12:30. Returns None.
@@ -22,20 +27,18 @@ def start() -> None:
         print(f'Current Time : {current_time} | Minute Time : {minute_time} ~ Type: {type(minute_time)} | print time : {print_time}')
         print(f'{minute_time == "30"} | {current_time not in list(time_dict.keys())} | {13 > int(current_time[0:2]) > 5}')
         time.sleep(5)
-        if minute_time == '30' and current_time not in list(time_dict.keys()) and 13 > int(current_time[0:2]) > 5:
-            json_user_path = Path('C:\\Users\\Kenneth Albarillo\\The_BORED_Folder\\API Fun Stuff\\Telegram\\Stock_Market_Bot_v2\\time_sys\\storing\\users.json')
-            json_stock_path = Path('C:\\Users\\Kenneth Albarillo\\The_BORED_Folder\\API Fun Stuff\\Telegram\\Stock_Market_Bot_v2\\time_sys\\storing\\stocks.json')
+        if minute_time == '30' and current_time not in list(time_dict.keys()):
             time_dict[current_time] = True
             print("WENT THROUGH!")
-            with open(json_user_path) as file:
+            with open(USER_PATH) as file:
                 user_information: dict = json.load(file)
-            with open(json_stock_path) as file:
+            with open(STOCK_PATH) as file:
                 old_stock_price: dict = json.load(file)
             for chat_id, user_tuple in user_information.items():
                 new_stock_price: dict = stock_price.scrap_yahoo(user_tuple[1])
                 msg: str = format_msg.format_update_msg(current_time, new_stock_price, old_stock_price, user_tuple[0])
                 send_msg.send(msg, chat_id)
-            store_data.store_stocks(json_stock_path, new_stock_price)
+            store_data.store_stocks(STOCK_PATH, new_stock_price)
         message_update()
 
 
@@ -44,8 +47,7 @@ def update_stock_data(stock_list : list) -> None:
     update_stock_data updates the stock data with the most current data.
     """
     stock_dict : dict = stock_price.scrap_yahoo(stock_list)
-    json_path = Path('C:\\Users\\Kenneth Albarillo\\The_BORED_Folder\\API Fun Stuff\\Telegram\\Stock_Market_Bot_v2\\time_sys\\storing\\stocks.json')
-    store_data.store_stocks(json_path, stock_dict)
+    store_data.store_stocks(STOCK_PATH, stock_dict)
 
 
 def message_update() -> None:
@@ -54,12 +56,10 @@ def message_update() -> None:
     """
     messages = recieve_msg.check_incoming_msg()
     if messages:
-        json_user_path = Path('C:\\Users\\Kenneth Albarillo\\The_BORED_Folder\\API Fun Stuff\\Telegram\\Stock_Market_Bot_v2\\time_sys\\storing\\users.json')
-        with open(json_user_path) as file:
+        with open(USER_PATH) as file:
             user_dict = json.load(file)
         sorted_user_dict, user_commands_list_tuple = sort_users.sort_users(user_dict, messages)
-        json_path = Path('C:\\Users\\Kenneth Albarillo\\The_BORED_Folder\\API Fun Stuff\\Telegram\\Stock_Market_Bot_v2\\time_sys\\storing\\users.json')
-        store_data.store_users(json_path, sorted_user_dict)
+        store_data.store_users(USER_PATH, sorted_user_dict)
 
 
 # update_data(['JPM', 'SAFE', 'AAPL'])
